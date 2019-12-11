ce812c82ddc2a7c03869756aa2d225ef81d0ae45
Alexandre Figueiredo <17xande@gmail.com>
2019-12-11 09:44:50 +0200

    correct example

--- _examples/player.go
+++ _examples/player.go
@@ -1,13 +1,20 @@
 package main
 
-import "github.com/17xande/go-omxplayer"
+import (
+	"log"
+
+	"github.com/17xande/go-omxplayer"
+)
 
 func main() {
-	omx := go-omxplayer.NewOMXPlayer("file.mp4", false, false, nil)
+	omx, err := omxplayer.NewOMXPlayer("file.mp4", false, false, nil)
+	if err != nil {
+		panic(err)
+	}
 	waiting := make(chan string)
 	defer close(waiting)
 	if err := omx.Open(waiting); err != nil {
 		log.Println("Error trying to open OMXPlayer: ", err)
 	}
-	done := <-waiting
+	<-waiting
 }
